a0d255430fb6c7b6ac6dd0b15efb277cfa9cc738
Brian Chen <brianchen0810@gmail.com>
2018-01-18 22:46:41 -0500

    Cleanup gyro straight

--- src/org/usfirst/frc/team321/robot/commands/UseDrivetrain.java
+++ src/org/usfirst/frc/team321/robot/commands/UseDrivetrain.java
@@ -10,13 +10,11 @@ public class UseDrivetrain extends Command {
     public static boolean gyroDrive = false;
     private boolean hasFinished;
     private double tolerance = 0.1, error;
-    private boolean straight;
     private double targetHeading;
 
     public UseDrivetrain() {
         requires(Robot.leftDrive);
         requires(Robot.rightDrive);
-        straight = false;
         hasFinished = false;
     }
 
@@ -30,23 +28,20 @@ public class UseDrivetrain extends Command {
         double turn = Robot.oi.drive.getRightXAxisValueNormalized();
         double leftMotorSpeed = 0, rightMotorSpeed = 0;
 
-        if (!straight) {
-            targetHeading = Robot.sensors.getFusedHeading();
-        }
-
         if (Math.abs(forward) > tolerance || Math.abs(turn) > tolerance) {
             if (Math.abs(turn) > tolerance) {
-                straight = false;
+                targetHeading = Robot.sensors.getFusedHeading();
                 leftMotorSpeed = Utilities.limit(forward + turn, 1);
                 rightMotorSpeed = Utilities.limit(forward - turn, 1);
             } else {
-                straight = true;
                 error = targetHeading - Robot.sensors.getFusedHeading();
                 error *= 0.2;
 
                 leftMotorSpeed = Utilities.limit(forward + error, 1);
                 rightMotorSpeed = Utilities.limit(forward - error, 1);
             }
+        } else {
+            targetHeading = Robot.sensors.getFusedHeading();
         }
 
         Robot.leftDrive.setMotors(ControlMode.PercentOutput, leftMotorSpeed);
